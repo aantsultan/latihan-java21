311046cf7822da75269c01ae8dfd17a051050ee7
A-Ant Sultan Rahmanya <a-ant.sultan@sakura-system.co.id>
2025-08-26 18:22:34 +0700

    # latihan-java21, update md and add initial test class java21-spring-ai

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Latihan Java 21
 
 This project is used for exercise and find modern java solutions
+
+Specification :
+
+    1. Java 21
+    2. Graal VM
+    3. Spring 3.5.x
---
+++ latihan-java21-spring-ai/src/test/java/id/latihan/java21/spring/ai/LatihanJava21SpringAiTest.java
@@ -0,0 +1,12 @@
+package id.latihan.java21.spring.ai;
+
+import org.junit.jupiter.api.Test;
+
+class LatihanJava21SpringAiTest {
+
+    @Test
+    void text(){
+        System.out.println("Hello World");
+    }
+
+}
